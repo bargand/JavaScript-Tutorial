e545da1c2e876be16982edacd3ed2dcde86cc5bc
Debargha Nandi <debarghanandi.india@gmail.com>
2024-02-16 00:18:45 +0530

    Exercises

---
+++ code with harry/Full JS Course/Exercise22.js
@@ -0,0 +1,9 @@
+// Write a JavaScript program to remove a character at the specified position in a given string and return the modified string.
+function removeCharacter() {
+	let originalString = "python";
+	newString = originalString.replace("n", "");
+	console.log(newString);
+}
+
+removeCharacter();
+
