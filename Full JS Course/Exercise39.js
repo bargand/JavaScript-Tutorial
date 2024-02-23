25c590882638ad597afafd9b2215ba8eb5b364dd
Debargha Nandi <debarghanandi.india@gmail.com>
2024-02-23 22:44:35 +0530

    JS

--- Full JS Course/Exercise39.js
+++ Full JS Course/Exercise39.js
@@ -1,15 +1,22 @@
 //Write a JavaScript program to find the closest value to 100 from two numerical values.
 
-let closestNum =(x,y)=>{
-    let num1 = 100-x
-    let num2 = 100-y
+let closestNum = (x, y) => {
+  if (x != y) {
+    let num1 = Math.abs(100 - x);
+    let num2 = Math.abs(100 - y);
 
-    // console.log(Math.min(num1 , num2))
-    if (num2 > num1){
-        console.log(`${x} is closest of 100`)
+    if (num1 < num2) {
+      console.log(`${x} is your closest number of 100`);
+    } else if (num2 < num1) {
+      console.log(`${y} is your closest number of 100`);
+    } else {
+      return 0;
     }
-    else{
-        console.log(`${y} is closest of 100`)
-    }
-}
-closestNum(10, 99)
+  } else {
+    console.log('Please enter the different number')
+  }
+};
+closestNum(10, 99);
+closestNum(100, 99);
+closestNum(0, 99);
+closestNum(10, 10);
