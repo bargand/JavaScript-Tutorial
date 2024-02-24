2e2b3fa8310dfb3a22229290ff3bc1e79302d088
Debargha Nandi <debarghanandi.india@gmail.com>
2024-02-24 21:57:42 +0530

    JS

--- Full JS Course/Exercise40.js
+++ Full JS Course/Exercise40.js
@@ -1,20 +1,17 @@
 //Write a JavaScript program to check whether two numbers are in the range 40..60 or 70..100 inclusive.
 
-
-
-function numbers_ranges(x, y) {
-    if ((x >= 40 && x <= 60 && y >= 40 && y <= 60) 
-        || 
-        (x >= 70 && x <= 100 && y >= 70 && y <= 100))
-       {
-      return true;
-       } 
-      else 
-       {
-      return false;
-    }
+/*
+ */
+let NumberRange = (x, y) => {
+  if (
+    (40 <= x && x <= 60 && 40 <= y && y <= 60) ||
+    (70 <= x && x <= 100 && 70 <= y && y <= 100)
+  ) {
+    return true;
+  } else {
+    return false;
   }
-  
-  console.log(numbers_ranges(44, 56));
-  console.log(numbers_ranges(70, 95));
-  console.log(numbers_ranges(50, 89));
+};
+console.log(NumberRange(44, 56));
+console.log(NumberRange(70, 95));
+console.log(NumberRange(50, 89));
---
+++ Full JS Course/JS Advance.js
@@ -0,0 +1,44 @@
+//IIFE = iimmidietly invoke function
+
+let sleep = async () => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(45);
+    }, 1000);
+  });
+};
+//jodi ami sudhu let a abong let b likhe print korar chesta kortam tahole error asto, error jate na ase tr jonno amra iife er bebohar jate 1 ta function 2 to variable er moddhei chole.
+(async function main() {
+  let a = await sleep();
+  let b = await sleep();
+  console.log(a);
+  console.log(a + b);
+})();
+
+//destructureing in array
+
+//jodi sudhu eta kore rekhe ditam to bas x er y er man ta print hoto, kintu niche jeta korechi ota korle x ar y er man nie seta akta array er moddhe rekhe baki gulo alada variable er moddhe rekhe debe.
+/*
+let [x, y] = [1, 2, 3, 4, 5, 6, 7];
+console.log(x, y)
+*/
+
+//jodi ...rest na likhe sudhu rest lekha hoto tahole 1, 2 ,3 print hoto baki gulo print hoto na.
+(async function main2() {
+  let [c, d, ...rest] = [1, 2, 3, 4, 5, 6, 7];
+  console.log(c, d, rest);
+})();
+
+
+//destructuring in object
+
+(async function main3(){
+    let obj = {
+        a : 1, 
+        b : 2, 
+        c : 3, 
+        d : 4
+    }
+    let {a, b, c} = obj
+    console.log(a, b, c)
+})()
