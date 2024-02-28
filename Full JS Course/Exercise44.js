1256a1fc02fbec3accc4b3d55c329c3635d3493b
Debargha Nandi <debarghanandi.india@gmail.com>
2024-02-28 16:37:09 +0530

    JS

---
+++ Full JS Course/Exercise44.js
@@ -0,0 +1,22 @@
+//Write a program to check whether a specified character exists between the 2nd and 4th positions in a given string.
+
+function check_char(str1, char)
+ {
+  ctr = 0;
+  for (let i = 0; i < str1.length; i++)
+  {
+    if ((str1.charAt(i) == char) && (i >= 1 && i <= 3))
+    {
+        ctr=1;
+        break;
+    }
+   }
+   if (ctr==1) return true;
+   return false;
+}
+console.log(check_char("Python", "a"));
+console.log(check_char("JavaScript", "a"));
+console.log(check_char("Console", "l"));
+console.log(check_char("Console", "C"));
+console.log(check_char("Console", "e"));
+console.log(check_char("JavaScript", "S"));
