5ced4fab7698a27104d1e0322da4063b72f58ab9
Debargha Nandi <debarghanandi.india@gmail.com>
2024-03-07 20:07:49 +0530

    JS

---
+++ Full JS Course/Exercise49.js
@@ -0,0 +1,14 @@
+//Write a JavaScript program to create one string of specified copies (positive numbers) of a given string.
+
+
+
+function string_copies (str, n) 
+{
+  if (n < 0)
+    return false;
+  else
+  return str.repeat(n);
+}
+console.log(string_copies("abc", 5));
+console.log(string_copies("abc", 0));
+console.log(string_copies("abc", -2));
