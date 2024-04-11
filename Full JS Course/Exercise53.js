2b5147e2cd399cf70b69c00c5ae8fd6b000150a5
Debargha Nandi <debarghanandi.india@gmail.com>
2024-04-12 00:22:45 +0530

    JS

--- Full JS Course/Exercise53.js
+++ Full JS Course/Exercise53.js
@@ -1,2 +1,15 @@
 //Print all even numbers from 0 – 10
 
+let x = 2;
+while (x <= 10) {
+  console.log(x);
+  x += 2;
+}
+
+//Print all odd numbers from 0 – 10
+
+let y = 1;
+while (y <= 10) {
+  console.log(y);
+  y += 2;
+}
