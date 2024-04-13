10032e5ecf15dd760a874b623de0f3b848f051a6
Debargha Nandi <debarghanandi.india@gmail.com>
2024-04-13 21:17:16 +0530

    js

---
+++ Full JS Course/Exercise55.js
@@ -0,0 +1,15 @@
+//Calculate the sum of numbers within an array
+
+let numbers = ([a,b,c,d,e]) =>{
+    return (a+b+c+d+e)
+}
+result = numbers([1,2,3,4,5])
+console.log(result)
+
+const number2 = [1,2,3,4,5]
+let myFunc = (number3)=>{
+    for (let number3 = 0; number3 < number2.length; number3++) {
+        console.log(myFunc(number2))
+    }
+}
+// myFunc()
