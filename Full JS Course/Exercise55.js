ce1ada4cfd99cfc902283074e6ce52ddc6adf8bb
Debargha Nandi <debarghanandi.india@gmail.com>
2024-04-15 20:24:31 +0530

    JS

--- Full JS Course/Exercise55.js
+++ Full JS Course/Exercise55.js
@@ -6,10 +6,10 @@ let numbers = ([a,b,c,d,e]) =>{
 result = numbers([1,2,3,4,5])
 console.log(result)
 
-const number2 = [1,2,3,4,5]
-let myFunc = (number3)=>{
-    for (let number3 = 0; number3 < number2.length; number3++) {
-        console.log(myFunc(number2))
-    }
+
+const a = [1,2,3,4,5]
+
+for (let i = 0; i < a.length; i++) {
+    console.log(a)
+    i+=1
 }
-// myFunc()
