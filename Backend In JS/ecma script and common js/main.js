e368a6754a06588fa271e92c20fe6b9d15aa1fd2
Debargha Nandi <debarghanandi.india@gmail.com>
2024-04-18 21:49:53 +0530

    JS

--- Backend In JS/ecma script and common js/main.js
+++ Backend In JS/ecma script and common js/main.js
@@ -1,3 +1,10 @@
-import { a } from "./mod.js";
+import { a, b, c, d ,e } from "./mod.js";
+import obj from "./mod.js"
 
 console.log(a)
+
+// the sum of the a ,b,c,d,e is
+
+console.log(a+b+c+d+e)
+
+console.log(obj)
