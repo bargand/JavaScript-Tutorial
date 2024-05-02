22450dd85e8d7316776f411a15434adef0d57986
Debargha Nandi <debarghanandi.india@gmail.com>
2024-05-02 23:06:27 +0530

    JS

--- Full JS Course/Exercise56.js
+++ Full JS Course/Exercise56.js
@@ -1,6 +1,5 @@
 //Write a JavaScript program to test whether a string ends with "Script". The string length must be greater than or equal to 6.
 
-
 let string = "JavaScript"
 let length = string.length
 
