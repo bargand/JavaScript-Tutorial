8743e46f5b4b082c815b5b870ebb38597367fe6f
Debargha Nandi <debarghanandi.india@gmail.com>
2024-05-13 23:08:59 +0530

    ReactJS

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import "./App.css"
 
 function App() {
+  const [value, setValue] = useState(0)
   return (
+    <>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        {value}
+      </div>
+      <button className='btn' onClick={()=>{
+        setValue(value + 1)
+      }}>Increase Here</button>
+
+      <button className='btn' onClick={()=>{
+        setValue(value - 1)
+      }}>Decrease Here</button>
     </div>
+    </>
   );
 }
 
